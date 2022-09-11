3bff88eafd11f510682257b7783cc646281d0e35
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-09-11 18:25:16 +0700

    feat: add Search box and Search cryptocurrencies in the Cryptocurrencies component
    search crypto using useEffect hook with user input
    
    Need to implement redux store to avoid useEffect hook in React cause useEffect sucks

--- src/components/Cryptocurrencies.tsx
+++ src/components/Cryptocurrencies.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, Row, Col, Input, Spin } from 'antd';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 import { Link } from 'react-router-dom';
@@ -9,19 +9,51 @@ type T = {
   [key: string]: number | string;
 };
 
+interface TCrypto extends T {
+  name: string;
+}
+
 const Cryptocurrencies = ({ simplified }: Props) => {
   //If simplified is true then display only top 10 currencies, if not display 100
   const count = simplified ? 10 : 100;
-  const { data, isFetching, isLoading, isSuccess } = useGetCryptosQuery(count);
-  const [cryptos, setCryptos] = useState<T[]>(data?.data?.coins);
+  const {
+    data: cryptoList,
+    isFetching,
+    isLoading,
+    isSuccess,
+  } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState<T[]>(cryptoList?.data?.coins);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  //TODO implement redux to avoid useEffect complication
+  useEffect(() => {
+    if (!cryptoList) return;
+    const filterData: T[] = cryptoList?.data?.coins.filter((coin: TCrypto) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setCryptos(filterData);
+  }, [searchTerm, cryptoList]);
+
   if (isFetching && isLoading) return <Spin size='large' className='loader' />;
   // After finish fetching and loading, the component state (cryptos) might not been set correctly so we add the below statement to make sure the internal state get set properly with the data return from API
 
   if (!cryptos && isSuccess) {
-    setCryptos(data.data.coins);
+    setCryptos(cryptoList.data.coins);
   }
+
   return (
     <>
+      {/* Add search box only when not in simplified  */}
+      {!simplified && (
+        <div className='search-crypto'>
+          <Input
+            placeholder='Search Cryptocurrencies'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
+
       <Row gutter={[32, 32]} className='crypto-card-container'>
         {cryptos?.map((currency) => (
           <Col key={currency.uuid} className='crypto-card'>
