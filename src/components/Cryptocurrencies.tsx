02479eddfb068d29b23fb361381f301b601ba019
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-09-11 16:33:07 +0700

    fix: :bug: logic to make sure crypto state is set correctly after the request has finished

--- src/components/Cryptocurrencies.tsx
+++ src/components/Cryptocurrencies.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Card, Row, Col, Input } from 'antd';
+import { Card, Row, Col, Input, Spin } from 'antd';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 import { Link } from 'react-router-dom';
 import millify from 'millify';
@@ -12,13 +12,18 @@ type T = {
 const Cryptocurrencies = ({ simplified }: Props) => {
   //If simplified is true then display only top 10 currencies, if not display 100
   const count = simplified ? 10 : 100;
-  const { data, isFetching } = useGetCryptosQuery(count);
+  const { data, isFetching, isLoading, isSuccess } = useGetCryptosQuery(count);
   const [cryptos, setCryptos] = useState<T[]>(data?.data?.coins);
-  if (isFetching && isLoading) return <Spin size='large' className='loader' />; 
+  if (isFetching && isLoading) return <Spin size='large' className='loader' />;
+  // After finish fetching and loading, the component state (cryptos) might not been set correctly so we add the below statement to make sure the internal state get set properly with the data return from API
+
+  if (!cryptos && isSuccess) {
+    setCryptos(data.data.coins);
+  }
   return (
     <>
       <Row gutter={[32, 32]} className='crypto-card-container'>
-        {cryptos?.map(currency => (
+        {cryptos?.map((currency) => (
           <Col key={currency.uuid} className='crypto-card'>
             <Link to={`/crypto/${currency.uuid}`}>
               <Card
