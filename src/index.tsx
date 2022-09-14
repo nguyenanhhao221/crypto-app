9254b028c2efe74677cf9e25a9813c129b0f161d
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-09-15 00:55:35 +0700

    fix: :bug: adjust ant library cause webpack to issue problem

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.min.css';
 import store from './app/store';
 import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(
