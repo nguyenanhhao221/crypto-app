f3de20c9c64abd3f9aa5c19bf65776b50d1917a1
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-09-14 23:21:36 +0700

    feat: :sparkles: CryptoDetail for each Crypto
    Set detail info about each cripto with its ID to display specific info about it using API call

--- src/services/cryptoApi.ts
+++ src/services/cryptoApi.ts
@@ -13,7 +13,7 @@ export const cryptoApi = createApi({
   reducerPath: 'cryptoApi', //will be use in the store to define the key of the reducer slice
   baseQuery: fetchBaseQuery({ baseUrl }), // similar to fetch API
   //each case of builder will be a method or different call to the api we want to call
-  endpoints: builder => ({
+  endpoints: (builder) => ({
     //Example of GET request, the query need to be a function which return an object contain the final API endpoint and related options to be called
     getCryptos: builder.query({
       query: (count: number = 10) => ({
@@ -22,7 +22,17 @@ export const cryptoApi = createApi({
         headers: cryptoApiHeaders,
       }),
     }),
+    // Api call to get info about specific coin with coin id
+    getCryptoDetail: builder.query({
+      query: ({ coinId, timePeriod }) => ({
+        url: `${baseUrl}/coin/${coinId}`,
+        headers: cryptoApiHeaders,
+        params: {
+          timePeriod,
+        },
+      }),
+    }),
   }),
 });
 //RTK will automatically generate these hook to refer to our call based on the name we given in the builder method
-export const { useGetCryptosQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailQuery } = cryptoApi;
