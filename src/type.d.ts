7719dda5bc5f0147aad46cb411a3d06025898dcc
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-09-21 16:44:54 +0700

    refactor: :label: add separate Type file for TypeScript

---
+++ src/type.d.ts
@@ -0,0 +1,15 @@
+//* type of exchange object return by API, make sure to update if APi changes
+export type TExchange = {
+  country: string;
+  description: string;
+  has_trading_incentive: boolean;
+  id: string;
+  image: string;
+  name: string;
+  trade_volume_24h_btc: number;
+  trade_volume_24h_btc_normalized: number;
+  trust_score: number;
+  trust_score_rank: number;
+  url: string;
+  year_established: number;
+};
