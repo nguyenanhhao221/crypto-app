686bcc4062f930c0b16ec8d52f99ae32de23d922
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-10-19 23:37:41 +0700

    switch to pnpm

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,6 @@ import morgan from 'morgan';
 import getCryptoRouter from './routes/getCryptoRouter';
 import getCryptoNewsRouter from './routes/getCryptoNewsRouter';
 import getCryptoExchangesRouter from './routes/getCryptoExchangesRouter';
-
 dotenv.config();
 
 const app = express();
